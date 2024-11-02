aa63be8b455706315bbeae4564d7a398d75734ca
binqnb18 <hoangvu180597@gmail.com>
2024-11-03 00:03:21 +0700

    frontend

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { PlayerContextProvider } from './context/PlayerContext';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <PlayerContextProvider>
+        <App />
+      </PlayerContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
